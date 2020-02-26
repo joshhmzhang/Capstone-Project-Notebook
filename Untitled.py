26ff2014ddef5b8236cf64ec8d13e2771da06df2
Josh ZHANG <52661411+joshhmzhang@users.noreply.github.com>
2020-02-26 22:12:08 +1100

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # capstone project
+
+# In[3]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
